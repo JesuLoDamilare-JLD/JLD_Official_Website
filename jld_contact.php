8e7ef40323c5ed4f11812dac101621e1c973e3ff
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-02-21 00:56:38 +0000

    form update

--- jld_contact.php
+++ jld_contact.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JLD Aveo+ Ventures</title>
-    <link rel="stylesheet" href="jld.css">
+    <link rel="stylesheet" href="jld_contact.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Akronim|Faster One' rel='stylesheet'>
@@ -16,9 +16,14 @@
 <body>
     <header>
         <div class="navdiv">
+            <a href="#" class="toggle-button">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </a>
             <img class="navlogo" src="jld.png" alt="JLD logo">
             <ul class="navul">
-                <li class="active"><a href="jld_home.html"><i class="fas fa-home"></i>Insight</a></li>
+                <li class="active"><a href="index.html"><i class="fas fa-home"></i>Insight</a></li>
                 <li><a href="jld_about.html"><i class="fas fa-info-circle"></i>Essence</a></li>
                 <li><a href="jld_services.html"><i class="fas fa-cogs"></i>Solutions</a>
                     <div class="sub-menu-1">
@@ -40,90 +45,89 @@
         </div>
     </header>
 
-    <body>
-        <h2>Contact Us</h2>
-        <p>Thank you for your interest in JLD Aveo Plus Ventures. If you have any questions or comments about our services, please feel free to contact us using the information below: </p>
-        <p>Phone: (+234)-913-184-3907 <strong> | </strong> (+44) 0777-825-2714</p>
-        <p>Email: contactus@jldaveoplus.com</p>
-        <form action="jld_contact.php" method="post">
-            <label for="fName">First Name:</label><br>
-            <input type="text" name="fName" id="fName" required><br><br>
-            <label for="lName">Last Name:</label><br>
-            <input type="text" name="lName" id="lName" required><br><br>
-            <label for="phone">Phone Number:</label><br>
-            <input type="tel" name="phone" id="phone" required><br><br>
-            <label for="email">Email:</label><br>
-            <input type="email" name="email" id="email" required><br><br>
-            <label for="comment">How can we assist you?</label><br>
-            <textarea name="comment" id="comment" cols="50" rows="10"></textarea><br>
-            <input type="submit" name="submit" id="submit" value="submit">
 
-        </form>
-        <p>Planning a visit to our main office in Abuja FCT, Nigeria? Great! Start at Kuje junction. From there, the embedded map on this page will guide you straight to us. Just follow the route from Kuje junction and you’ll find us waiting to welcome you!</p>
-        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1970.9839148662156!2d7.229109238748657!3d8.882582947836044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x104e65c07945befd%3A0x46a4ce0766a5f8a9!2sKuje%2C%20Nigeria!3m2!1d8.8821438!2d7.2275336!4m5!1s0x104e65ce7518bb6b%3A0x6d432a8c92f3b7fe!2sjld%20aveo%2B%20ventures!3m2!1d8.882439!2d7.2331002!5e0!3m2!1sen!2suk!4v1701548493611!5m2!1sen!2suk" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <?php
+    /* if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fName = $_POST["fName"];
+    $lName = $_POST["lName"];
+    $phone = $_POST["phone"];
+    $email = $_POST["email"];
+    $comment = $_POST["comment"];
+
+    $to = "dareolumuyiwa@jldaveoplus.com";
+    $subject = "New contact form submission";
+    $message = "First Name: $fName\n Last Name: $lName\n Phone Number:$phone\n Email:$email\n Comment:$comment";
+    $headers = "From: www.jldaveoplus.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "<div class='msg_delivered'>Thank you for submission. We will get back to you soon.</div>";
+        echo "<div class='msg_delivered'>Return to Home Page</div>";
+    } else {
+        echo "<div class='msg_delivered'>Sorry, there was a problem with your submission. Please try again.</div>";
+        echo "<div class='msg_delivered'>Return to Contact Us Page</div>";
+    }
+} */
+    // echo '<link rel="stylesheet" href="jld_contact.css">';
 
 
-        <footer>
-            <hr class="footerHr">
-            <div class="allfooter">
-                <div class="footerdiv">
-                    <img class="footerjldlogo" src="jld.png" alt="JLD Aveo+ Ventures Logo">
-                    <p>Seamless and comprehensive technology solutions for individuals and businesses, specializing in ICT, printing, branding, computer training, software development, and website development, ensuring customer satisfaction and staying competitive.</p>
-                </div>
 
-                <div class="footerlists">
-                    <ul>
-                        <li><a href="jld_about.html">About Us<span>&#62;</span>
-                                <hr>
-                            </a></li>
-                        <li><a href="jld_career.html">Careers<span>&#62;</span>
-                                <hr>
-                            </a></li>
-                        <li><a href="jld_contact.html">Contact Us<span>&#62;</span>
-                                <hr>
-                            </a></li>
-                        <li><a href="jld_privacy.html">Privacy and Terms<span>&#62;</span>
-                                <hr>
-                            </a></li>
-                    </ul>
-                </div>
-                <div class="footeraddress">
-                    <img src="map.png" alt="Google Map Logo">
-                    <p>Plot 400, Staff Quarters <br>
-                        Opp Road Safety, Beside General Hospital <br>
-                        Kuje, 900105, Abuja (FCT) <br>
-                        Nigeria
-                    </p><br>
-                    <p>De-Royal Plaza, Block R1/R2 <br>
-                        Beside Sajumah Plaza <br>
-                        Kuje 900105, Abuja (FCT) <br>
-                        Nigeria</p>
-                </div>
-                <div class="footerhandles">
-                    <ul>
-                        <li><a href="https://web.facebook.com/profile.php?id=100070949910985" target="_blank"><img src="fb.png" alt="Facebook Logo"></a></li>
-                        <li><a href="https://instagram.com/jld_aveoplus?igshid=NTc4MTIwNjQ2YQ==" target="_blank"><img src="ig.png" alt="Instagram Logo"></a></li>
-                        <li><a href="https://www.linkedin.com/company/jldaveoplus/" target="_blank"><img src="Lin.png" alt="Linkedin Logo"></a></li>
-                        <li><img src="twit.png" alt="Twitter Logo"></li>
-                        <li><img src="wtsp.png" alt="WhatsApp Logo"></li>
-                        <li><img src="ytb.png" alt="YouTube Logo"></li>
-                    </ul>
-                </div>
+    echo "<div class='msg_delivered'>Thank you for submission. We will get back to you soon.</div>";
+    echo "<div class='msg_delivered'>Return to Home Page</div>";
+    echo "<div class='msg_delivered'>Sorry, there was a problem with your submission. Please try again.</div>";
+    echo "<div class='msg_delivered'>Return to Contact Us Page</div>";
+
+    ?>
+
+    <footer>
+        <hr class="footerHr">
+        <div class="allfooter">
+            <div class="footerdiv">
+                <img class="footerjldlogo" src="jld.png" alt="JLD Aveo+ Ventures Logo">
+                <p>Seamless and comprehensive technology solutions for individuals and businesses, specializing in ICT, printing, branding, computer training, software development, and website development, ensuring customer satisfaction and staying competitive.</p>
             </div>
-            <p>Copyright &copy; 2023 JLD Aveo+ Ventures. All rights reserved.</p>
-        </footer>
 
-    </body>
+            <div class="footerlists">
+                <ul>
+                    <li><a href="jld_about.html">About Us<span>&#62;</span></a>
+                        <hr>
+                    </li>
+                    <li><a href="jld_services.html">Our Services<span>&#62;</span></a>
+                        <hr>
+                    </li>
+                    <li><a href="jld_career.html">Career Path<span>&#62;</span></a>
+                        <hr>
+                    </li>
+                    <li><a href="jld_contact.html">Contact Us<span>&#62;</span></a>
+                        <hr>
+                    </li>
+                </ul>
+            </div>
+            <div class="footeraddress">
+                <img src="map.png" alt="Google Map Logo">
+                <p>Plot 400, Staff Quarters <br>
+                    Opp Road Safety, Beside General Hospital <br>
+                    Kuje, 900105, Abuja (FCT) <br>
+                    Nigeria
+                </p><br>
+                <p>De-Royal Plaza, Block R1/R2 <br>
+                    Beside Sajumah Plaza <br>
+                    Kuje 900105, Abuja (FCT) <br>
+                    Nigeria</p>
+            </div>
+            <div class="footerhandles">
+                <ul>
+                    <li><a href="https://web.facebook.com/profile.php?id=100070949910985" target="_blank"><img src="fb.png" alt="Facebook Logo"></a></li>
+                    <li><a href="https://instagram.com/jld_aveoplus?igshid=NTc4MTIwNjQ2YQ==" target="_blank"><img src="ig.png" alt="Instagram Logo"></a></li>
+                    <li><a href="https://www.linkedin.com/company/jldaveoplus/" target="_blank"><img src="Lin.png" alt="Linkedin Logo"></a></li>
+                    <li><img src="twit.png" alt="Twitter Logo"></li>
+                    <li><img src="wtsp.png" alt="WhatsApp Logo"></li>
+                    <li><img src="ytb.png" alt="YouTube Logo"></li>
+                </ul>
+            </div>
+        </div>
+        <p>Copyright &copy; 2023 JLD Aveo+ Ventures. All rights reserved.</p>
+    </footer>
+    <script src="jld_toggle.js"></script>
+</body>
 
 </html>
-<?php
-if (isset($_POST["submit"])) {
-
-    $submit = filter_input(INPUT_POST, "comment", FILTER_SANITIZE_NUMBER_INT);
-
-    echo "Read: {$submit}";
-};
-
-
-
-?>
